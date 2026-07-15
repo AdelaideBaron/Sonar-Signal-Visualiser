0260c399a4d3ee94359d2e901e5aa2ad0da5d56b
AdelaideBaron <baronadelaide@hotmail.com>
2026-07-15 18:55:20 +0100

    setting panel size

--- sonar-console/src/main/java/com/addi/HomePanel.java
+++ sonar-console/src/main/java/com/addi/HomePanel.java
@@ -4,19 +4,16 @@ import javax.swing.*;
 import java.awt.*;
 
 
-public class HomePanel {
-
-    public JPanel panel = new JPanel();
+public class HomePanel extends JPanel {
 
     public HomePanel() {
+        setPreferredSize(new Dimension(800, 500));
 
-        panel.setBackground(Color.BLACK);
+        setBackground(Color.BLACK);
 
         JLabel title = new JLabel("SONAR CONSOLE");
         title.setForeground(Color.GREEN);
 
-
-
-        panel.add(title);
+        add(title);
     }
 }
--- sonar-console/src/main/java/com/addi/Main.java
+++ sonar-console/src/main/java/com/addi/Main.java
@@ -6,14 +6,11 @@ import java.awt.*;
 public class Main {
     static void main() {
         JFrame frame = new JFrame("sonar-console");
-
-
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         HomePanel homePanel = new HomePanel();
+        frame.setContentPane(homePanel);
 
-
-        frame.setContentPane(homePanel.panel);
         frame.pack();
         frame.setVisible(true);
     }
