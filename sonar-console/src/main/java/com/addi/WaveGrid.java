57afeca3a33513eece287b89afac3ae361553935
AdelaideBaron <baronadelaide@hotmail.com>
2026-07-15 19:23:07 +0100

    update wave grid size and tests

--- sonar-console/src/main/java/com/addi/WaveGrid.java
+++ sonar-console/src/main/java/com/addi/WaveGrid.java
@@ -5,23 +5,31 @@ import java.awt.*;
 
 public class WaveGrid extends JPanel {
 
+    private static final int GRID_SPACING = 25;
+
     public WaveGrid() {
-        setPreferredSize(new Dimension(500, 500));
-        setBackground(Color.DARK_GRAY);
+        setPreferredSize(new Dimension(700, 350));
+        setBackground(Color.BLACK);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.setColor(Color.GREEN);
+        g.setColor(new Color(0, 100, 0));
 
-        for (int x = 0; x < getWidth(); x += 30) {
+        // vertical grid
+        for (int x = 0; x < getWidth(); x += GRID_SPACING) {
             g.drawLine(x, 0, x, getHeight());
         }
 
-        for (int y = 0; y < getHeight(); y += 30) {
+        // horizontal grid
+        for (int y = 0; y < getHeight(); y += GRID_SPACING) {
             g.drawLine(0, y, getWidth(), y);
         }
+
+        // centre line (where the waveform will be drawn)
+        g.setColor(Color.GREEN);
+        g.drawLine(0, getHeight() / 2, getWidth(), getHeight() / 2);
     }
 }
