aa448346974e3ad3b65d66af2176a1f590cc70ce
AdelaideBaron <baronadelaide@hotmail.com>
2026-07-15 19:20:30 +0100

    adding wave grid and border

--- sonar-console/src/main/java/com/addi/HomePanel.java
+++ sonar-console/src/main/java/com/addi/HomePanel.java
@@ -7,13 +7,22 @@ import java.awt.*;
 public class HomePanel extends JPanel {
 
     public HomePanel() {
-        setPreferredSize(new Dimension(800, 500));
+        setLayout(new BorderLayout());
 
+        setPreferredSize(new Dimension(800, 500));
         setBackground(Color.BLACK);
 
         JLabel title = new JLabel("SONAR CONSOLE");
         title.setForeground(Color.GREEN);
+        title.setHorizontalAlignment(SwingConstants.CENTER);
 
-        add(title);
+        WaveGrid waveGrid = new WaveGrid();
+
+
+        add(title, BorderLayout.NORTH);
+        add(waveGrid, BorderLayout.CENTER);
+
+        setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+
     }
 }
