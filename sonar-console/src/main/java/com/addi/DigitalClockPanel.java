b58dfc625dda647c6994e4e134197849af3b68a7
AdelaideBaron <baronadelaide@hotmail.com>
2026-07-16 19:45:29 +0100

    creating digital clock

---
+++ sonar-console/src/main/java/com/addi/DigitalClockPanel.java
@@ -0,0 +1,44 @@
+package com.addi;
+
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class DigitalClockPanel extends JPanel {
+
+    private static final DateTimeFormatter TIME_FORMAT =
+            DateTimeFormatter.ofPattern("HH:mm:ss");
+
+    private final JLabel timeLabel = new JLabel();
+
+    public DigitalClockPanel() {
+        setBackground(Color.BLACK);
+
+        timeLabel.setForeground(Color.GREEN);
+        timeLabel.setFont(
+                new Font("Monospaced", Font.BOLD, 22)
+        );
+
+        add(timeLabel);
+
+        updateClock();
+
+        Timer timer = new Timer(
+                1000,
+                event -> updateClock()
+        );
+
+        timer.start();
+    }
+
+    private void updateClock() {
+        String currentTime =
+                LocalTime.now().format(TIME_FORMAT);
+
+        timeLabel.setText(currentTime);
+    }
+}
+
